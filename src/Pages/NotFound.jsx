784e6be34d5d34e0d6c9b87d1a2e53f7314bdf23
hossein <hosseinfalah2021@gmail.com>
2023-02-28 21:09:46 +0330

    added NotFound Page

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,6 +1,14 @@
 const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <div className="grid place-items-center">
+            <h1 className="font-IranSansDN text-emerald-700 text-2xl">صفحه مورد نظر پیدا نشد</h1>
+            <div className="max-w-md lg:max-w-xl my-6">
+                <img 
+                    src="/Assets/NotFound.jpg"
+                    className="max-w-full object-cover h-auto"
+                    alt="صفحه مورد نظر پیدا نشد" />
+            </div>
+        </div>
     )
 }
 
