de969e261e368a25f5a81bce7f94065654b995d3
hossein <hosseinfalah2021@gmail.com>
2023-03-01 17:00:20 +0330

    added Shop Page And Add SearchBar, SelectMenu

---
+++ src/Components/SelectBox/SelectBox.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import Select from "react-select";
+
+const SelectBox = () => {
+    const sortOption = [
+        {id: 1, label: 'مرتب سازی پیش فرض', value: 'Default'},
+        {id: 2, label: 'مرتب سازی بر اساس محبوبیت', value: 'Popularity'},
+        {id: 3, label: 'مرتب سازی بر اساس امتیاز', value: 'Score'},
+        {id: 4, label: 'مرتب سازی بر اساس آخرین', value: 'Latest'},
+        {id: 5, label: 'مرتب سازی بر اساس ارزان ترین', value: 'Cheapest'},
+        {id: 6, label: 'مرتب سازی بر اساس گران ترین', value: 'MostExpensive'}
+    ]
+
+    const [isClearable, setIsClearable] = useState(true);
+    const [isSearchable, setIsSearchable] = useState(true);
+    const [isDisabled, setIsDisabled] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    return (
+        <Select
+            className="w-2/5 font-IranSansDN text-amber-600"
+            defaultValue={sortOption[1]}
+            isDisabled={isDisabled}
+            isLoading={isLoading}
+            isClearable={isClearable}
+            isRtl={true}
+            isSearchable={isSearchable}
+            name="sort"
+            onChange={() => setIsLoading(true)}
+            options={sortOption}
+            placeholder="انتخاب کنید ..."
+        />
+    )
+}
+
+export default SelectBox
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,6 +1,49 @@
+import { useState } from "react";
+
+import { BsSearch } from "react-icons/bs";
+import { CiLocationOn } from "react-icons/ci";
+import { IoIosArrowBack } from "react-icons/io";
+
+import SelectBox from "../Components/SelectBox/SelectBox";
+
 const Shop = () => {
+    const [querySearch, setQuerySearch] = useState("");
+
     return (
-        <div>Shop</div>
+        <div className="container m-auto">
+            <div className="my-4">
+                <ul className="bg-[#52ac66] rounded-xl flex py-4 px-6">
+                    <li className="flex items-center text-white">
+                        <CiLocationOn size={25}/>
+                        <a href="!#" className="text-gray-200 mr-6 ml-3">خانه</a>
+                    </li>
+                    <li className="flex items-center text-white">
+                        <IoIosArrowBack className="ml-3" size={20}/>
+                        <a href="!#" className="text-gray-100">فروشگاه</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-x-6 my-6">
+                <div className="col-span-2">
+                    <div className="flex items-center justify-between bg-[#f8f9fa] py-6 px-4 rounded-xl">
+                        <div className="w-2/5 relative">
+                            <input 
+                                type="text" 
+                                className="w-full py-3 px-5 pr-10 rounded-2xl focus:outline-none ring-1 ring-green-500"
+                                value={querySearch} 
+                                onChange={(e) => setQuerySearch(e.target.value)}
+                                placeholder="جستجو در محصولات ..."
+                            />
+                            <BsSearch className="text-gray-500 absolute top-3 right-3" size={18}/>
+                        </div>
+                        <SelectBox/>
+                    </div>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
     )
 }
 
